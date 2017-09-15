4287212a8c30662e92f6c37f0d37dd1333f24382
AidasVaiksnoras <vaiksnorasaidas@gmail.com>
2017-09-15 16:50:38 +0300

    Add VideoPane with sample video (#4)

--- Foosball2text/Form1.cs
+++ Foosball2text/Form1.cs
@@ -1,20 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace Foosball2text
 {
     public partial class Form1 : Form
     {
+        Timer _timer;
+        private const int Fps = 30;
+        VideoCapture _capture;
+
         public Form1()
         {
             InitializeComponent();
+
+            _timer = new Timer();
+            //Frame Rate
+            _timer.Interval = 1000 / Fps;
+            _timer.Tick += new EventHandler(TimerTick);
+            _timer.Start();
+
+            _capture = new VideoCapture("../../sample.avi");
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            Mat m = _capture.QueryFrame();
+            if (m == null) return;
+            Image<Bgr, byte> resizedImage = m.ToImage<Bgr, byte>().Resize(pictureBox1.Width, pictureBox1.Height, Inter.Linear);
+            pictureBox1.Image = resizedImage.Bitmap;
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
         }
     }
 }
