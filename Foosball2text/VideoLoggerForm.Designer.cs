a455f7f0b622b6d56b5b50eed7b1fe464b25add0
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-09-20 15:03:40 +0300

    VideoLoggerForm creation

---
+++ Foosball2text/VideoLoggerForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Foosball2text
+{
+    partial class VideoLoggerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LatestLog = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Coordinates = new System.Windows.Forms.Label();
+            this.Speed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Left Goal Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Right Goal Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(315, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ball Disappeared Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(315, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ball Passed to";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(471, 211);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(145, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "has Disappeared";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(471, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Latest Log: ";
+            // 
+            // LatestLog
+            // 
+            this.LatestLog.AutoSize = true;
+            this.LatestLog.Location = new System.Drawing.Point(82, 102);
+            this.LatestLog.Name = "LatestLog";
+            this.LatestLog.Size = new System.Drawing.Size(33, 13);
+            this.LatestLog.TabIndex = 11;
+            this.LatestLog.Text = "None";
+            this.LatestLog.Click += new System.EventHandler(this.LatestLog_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(297, 186);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Ball coordinates:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Ball speed:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Coordinates
+            // 
+            this.Coordinates.AutoSize = true;
+            this.Coordinates.Location = new System.Drawing.Point(100, 9);
+            this.Coordinates.Name = "Coordinates";
+            this.Coordinates.Size = new System.Drawing.Size(25, 13);
+            this.Coordinates.TabIndex = 15;
+            this.Coordinates.Text = "Null";
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.Location = new System.Drawing.Point(100, 35);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(25, 13);
+            this.Speed.TabIndex = 16;
+            this.Speed.Text = "Null";
+            // 
+            // VideoLoggerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 324);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.Coordinates);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.LatestLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "VideoLoggerForm";
+            this.Text = "VideoLogger";
+            this.Load += new System.EventHandler(this.VideoLoggerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LatestLog;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Coordinates;
+        private System.Windows.Forms.Label Speed;
+    }
+}
---
+++ Foosball2text/VideoLoggerForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foosball2text
+{
+    public partial class VideoLoggerForm : Form
+    {
+        BindingList<String> logData = new BindingList<string>();
+
+        public VideoLoggerForm()
+        {
+            InitializeComponent();
+            logData.Add(VideoLoggerMessageDelivery.gameStart);
+            listBox1.DataSource = logData;
+            logData.ListChanged += new ListChangedEventHandler(list_ListChanged);
+        }
+
+        void list_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            LatestLogUpdate();
+        }
+
+        private void LatestLogUpdate()
+        {
+            LatestLog.Text = logData.Last();
+
+            int visibleItems = listBox1.ClientSize.Height / listBox1.ItemHeight;
+            listBox1.TopIndex = Math.Max(listBox1.Items.Count - visibleItems + 1, 0);
+        }
+
+        private void LatestLog_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logData.Add(VideoLoggerMessageDelivery.goalLeft);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            logData.Add(VideoLoggerMessageDelivery.goalRight);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            logData.Add(VideoLoggerMessageDelivery.dissapearedBall);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            logData.Add(VideoLoggerMessageDelivery.getPassMessage(textBox1.Text));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            logData.Add(VideoLoggerMessageDelivery.getDisappearMessage(textBox2.Text));
+        }
+
+        private void VideoLoggerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
