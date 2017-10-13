a2e5e84dc10dd94af70df05d4f79f79e73fb2b67
Tomas <tomas.ukrinas1997@gmail.com>
2017-10-13 03:04:30 +0300

    Add Username functionality

---
+++ Foosball2text/Login.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Foosball2text
+{
+    public class Login
+    {
+        public String _teamA { get; set; }
+        public String _teamB { get; set; }
+
+        private Database1DataSet _dataset;
+        private Database1DataSetTableAdapters.UserTableAdapter _tableAdapter;
+
+        public Login(Database1DataSet dataset, Database1DataSetTableAdapters.UserTableAdapter tableAdapter)
+        {
+            _dataset = dataset;
+            _tableAdapter = tableAdapter;
+        }
+
+        public int Loginas(String name)//Grazina 1 jeigu vartotojas yra naujas, 0 jeigu jau senas
+        {
+            _tableAdapter.Fill(_dataset.User);
+            for (int i = 0; i < _dataset.User.Count; i++)
+            {
+                if(_dataset.User[i].Name.Contains(name))
+                {
+                    return 0;
+                }
+            }
+            Register(name);
+            return 1;
+        }
+
+        public void Register(String name)
+        {
+            DataRow row = _dataset.User.NewRow();
+            row[0] = generateId();
+            row[1] = name;
+            row[2] = 0;
+            row[3] = 0;
+            _dataset.User.Rows.Add(row);
+            _tableAdapter.Update(_dataset);
+            _tableAdapter.Fill(_dataset.User);
+        }
+
+        private int generateId()
+        {
+            int i;
+            int id = 0;
+            for (i = 0; i < _dataset.User.Count; i++)
+            {
+                if ((int)_dataset.User[i][0] == id)
+                {
+                    id++;
+                }
+                else
+                {
+                    return id;
+                }
+            }
+            return id;
+        }
+    }
+}
---
+++ Foosball2text/LoginForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logic;
+
+namespace Foosball2text
+{
+    public partial class LoginForm : Form
+    {
+        private Login _login;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            _login = new Login(database1DataSet1, userTableAdapter1);
+        }
+
+        private void login1_Click(object sender, EventArgs e)
+        {
+            if (_login.Loginas(name1.Text) == 1) label1.Text = "Sukurtas naujas vartotojas " + name1.Text;
+            else label1.Text = "Prisijungta vardu " + name1.Text;
+            _login._teamA = name1.Text;
+            checkIfDoneNaming();
+        }
+
+        private void login2_Click(object sender, EventArgs e)
+        {
+            if (_login.Loginas(name2.Text) == 1) label2.Text = "Sukurtas naujas vartotojas " + name2.Text;
+            else label2.Text = "Prisijungta vardu " + name2.Text;
+            _login._teamB = name2.Text;
+            checkIfDoneNaming();
+        }
+
+        private void checkIfDoneNaming()
+        {
+            if((label1.Text != "")&&(label2.Text != ""))
+            {
+                
+            }
+        }
+    }
+}
