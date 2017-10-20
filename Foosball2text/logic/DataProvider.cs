ac7d086ac5c92b0e084cf15c28bc6dfe6971f8de
Aidas Vaikšnoras <aidas.vaiksnoras@mif.stud.vu.lt>
2017-10-20 22:14:10 +0300

    Add ignored files

---
+++ Foosball2text/logic/DataProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class UsersDataProvider
+    {
+        public List<User> Data { get; set; }
+        public User LeftUser { get; set; }
+        public User RightUser { get; set; }
+
+        public UsersDataProvider()
+        {
+            Data = new List<User>();
+            LeftUser = new User();
+            RightUser = new User();
+        }
+
+        public void LoadData()
+        {
+            Data = SerializationHandler.ReadFromXML<User>();
+        }
+
+        public void SaveData()
+        {
+            SerializationHandler.WriteToXML<User>(Data);
+        }
+
+        public User AddUser(string name)
+        {
+            if (!Data.Any(x => x.UserName == name))
+            {
+                User user = new User(name);
+                Data.Add(new User(name));
+                return user;
+            }
+            else
+                return GetUserData(name);
+        }
+
+        public User GetUserData(string name)
+        {
+            var users = Data.Where(x => x.UserName == name);
+            if (users.Count() == 1)
+                return users.Last();
+            else
+                return new User();
+        }
+
+
+
+    }
+}
---
+++ Foosball2text/logic/SerializationHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Logic
+{
+    class SerializationHandler
+    {   
+        private static string _fileName = "Users.XML";
+
+        public static List<T> ReadFromXML<T>()
+        {
+            List<T> list = new List<T>();
+            if (File.Exists("Users.XML"))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(List<T>));
+                using (FileStream fs = File.OpenRead(_fileName))
+                {
+                    list = (List<T>)deserializer.Deserialize(fs);
+                }
+            }
+            return list;
+        }
+
+        public static void WriteToXML<T>(List<T> list)
+        {
+            using (Stream fs = new FileStream(_fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                serializer.Serialize(fs, list);
+            }
+        }
+    }
+}
