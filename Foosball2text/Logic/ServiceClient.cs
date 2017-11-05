ac3ac6691e05938e08f53e1b7935195c95434c07
Aidas Vaikšnoras <aidas.vaiksnoras@mif.stud.vu.lt>
2017-11-05 21:09:31 +0200

    add missing file

---
+++ Foosball2text/Logic/ServiceClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+namespace Logic
+{
+    static public class ServiceClient
+    {
+        static HttpClient client = new HttpClient();
+        static StringBuilder stringBuilder = new StringBuilder();
+        static ServiceClient()
+        {
+            client.BaseAddress = new Uri("http://localhost:53506/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+        }
+    }
+}
