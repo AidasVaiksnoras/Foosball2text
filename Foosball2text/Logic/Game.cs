580dd3b535cfd5e64ef8fd87814405b7eea086c2
Aidas Vaikšnoras <aidas.vaiksnoras@mif.stud.vu.lt>
2017-11-15 16:31:40 +0200

    Add missing files

---
+++ Foosball2text/Logic/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Game
+    {
+        public string LeftUserName { get; set;}
+        public string RightUserName { get; set; }
+        public int LeftScore { get; set; }
+        public int RightScore { get; set; }
+    }
+}
