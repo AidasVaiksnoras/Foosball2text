7b1a4abf081cc5812982ea4bd7f251a4403f2d2c
Aidas Vaikšnoras <aidas.vaiksnoras@mif.stud.vu.lt>
2017-11-15 19:33:22 +0200

    Add missing file

---
+++ Foosball2text/Logic/OnScoredEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class OnScoredEventArgs : EventArgs
+    {
+        public Game Game { get; set; }
+
+        public OnScoredEventArgs(Game game)
+        {
+            Game = game;
+        }
+    }
+}
