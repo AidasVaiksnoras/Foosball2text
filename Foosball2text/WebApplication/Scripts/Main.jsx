9fbe15a1e781de0eb5c3d8d93101c106fd978c19
mnaujokas <mnaujokas@live.com>
2017-11-17 21:16:52 +0200

    Implement timed ajax request

--- Foosball2text/WebApplication/Scripts/Main.jsx
+++ Foosball2text/WebApplication/Scripts/Main.jsx
@@ -1,13 +1,50 @@
-﻿var CommentBox = React.createClass({
+﻿var result;
+var obj;
+
+$(function () {
+    var storiesInterval = 10 * 1000;
+    var fetchNews = function () {
+        console.log('Sending AJAX request...');
+        $.ajax({
+            type: "GET",
+            url: "api/CurrentGame",
+            data: {
+                user: 'success',
+                some: ['other', 'data']
+            }
+        }).done(function (msg) {
+            $(msg).appendTo("#edix");
+            console.log('success');
+            console.log(msg);
+            obj = JSON.parse(msg);
+            console.log(obj.LeftUserName);
+        }).fail(function () {
+            console.log('error');
+        }).always(function () {
+            // Schedule the next request after this one completes,
+            // even after error
+            console.log('Waiting ' + (storiesInterval / 1000) + ' seconds');
+            setTimeout(fetchNews, storiesInterval);
+        });
+    }
+
+    // Fetch news immediately, then every 10 seconds AFTER previous request finishes
+    fetchNews();
+});
+
+
+var CommentBox = React.createClass({
     render: function () {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
-      </div>
+                <h1>Comments</h1>
+                <h2> See console </h2>
+            </div>
         );
     }
 });
+
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={obj} />,
     document.getElementById('content')
 );
