42eae6577264a3d6c0218d089cca959d1df5f51e
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-11-19 00:46:43 +0200

    SQL connection string is now provided in settings (app setting)

--- SQL operations/ConnectionProvider.cs
+++ SQL operations/ConnectionProvider.cs
@@ -7,12 +7,17 @@ namespace SQL_operations
 {
     public class ConnectionProvider
     {
+        //public static SqlConnection GetConnection()
+        //{
+        //    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder("Server=tcp:universityprojects.database.windows.net,1433;Initial Catalog=foosball2text;Persist Security Info=False;User ID=warhatch;Password=Awalgato0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        //    return new SqlConnection(builder.ConnectionString);
+        //}
+
         public static SqlConnection GetConnection()
         {
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder("Server=tcp:universityprojects.database.windows.net,1433;Initial Catalog=foosball2text;Persist Security Info=False;User ID=warhatch;Password=Awalgato0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            return new SqlConnection(builder.ConnectionString);
+            string connectionString = Properties.Settings.Default.DatabaseConnectionString;
+            return new SqlConnection(connectionString);
         }
-        
 
     }
 }
