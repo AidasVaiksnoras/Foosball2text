b3ad413ab316786b253ac84d0790e1613a5dc86b
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-12-15 13:07:51 +0200

    Savepoint

--- Foosball2text/WebApplication/Helpers/DataProviderEF.cs
+++ Foosball2text/WebApplication/Helpers/DataProviderEF.cs
@@ -67,10 +67,13 @@ namespace WebApplication.Helpers
                 List<User> users = db.Users.Where(x => x.Username == userToUpdate.Username).ToList();
                 if (users.Count() == 1)
                 {
-                    users[0] = userToUpdate;
-                    //TODO change the way values are changed inside logic (specifically - MaxSpeed)
-                    db.Entry(users[0]).State = System.Data.Entity.EntityState.Modified;
-                    db.SaveChanges();
+                    if (users[0].GamesPlayed <= userToUpdate.GamesPlayed)
+                    {
+                        users[0] = userToUpdate;
+                        //FIXME doesn't update correctly
+                        db.Entry(users[0]).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
                 }
             }
         }
@@ -95,6 +98,17 @@ namespace WebApplication.Helpers
                     user = data[0];
             }
             return user;
+        }
+
+        public bool UserExists(string username)
+        {
+            using (var db = new EFModel())
+            {
+                List<User> data = db.Users.Where(x => x.Username == username).ToList();
+                if (data.Count() > 0)
+                    return true;
+            }
+            return false;
         }
 
         public List<Game> GetLastGames(int number)
