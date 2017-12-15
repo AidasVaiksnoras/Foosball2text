3ffe9f871d7c48b45a5dbb30d9fa6a43466dc3da
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-12-15 14:26:06 +0200

    Game Time brought back to SaveGameResults form

--- Foosball2text/Logic/User.cs
+++ Foosball2text/Logic/User.cs
@@ -7,8 +7,9 @@ namespace Logic
         public string UserName { get; set; }
         public int GamesPlayed { get; set; }
         public int GamesWon { get; set; }
-        public double MaxSpeed { get; set; }
         public int TotalGoals { get; set; }
+        public double MaxSpeed { get; set; }
+        TimeSpan TimePlayed { get; set; }
         public int RankPoints { get; set; }
 
         public User() {}
@@ -28,7 +29,7 @@ namespace Logic
             RankPoints = rankPoints;
         }
 
-        public void UpdateData(bool addGamePlayed, bool addGameWon, double gamesMaxSpeed, int addScore, TimeSpan gameTime , int rankChange)
+        public void UpdateData(bool addGameWon, double gamesMaxSpeed, int addScore, GameTime gameTime)
         {
             GamesPlayed++;
             if (addGameWon)
@@ -36,7 +37,8 @@ namespace Logic
             if (gamesMaxSpeed > MaxSpeed)
                 MaxSpeed = gamesMaxSpeed;
             TotalGoals += addScore;
-            RankPoints += rankChange;
+            TimeSpan gameTimeSpan = new TimeSpan(0, gameTime.min, gameTime.sec);
+            TimePlayed += gameTimeSpan;
         }
 
         public bool Equals(User other)
